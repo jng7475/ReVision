5fa786c095f0b97d51c64b4739d6ad7fb1f07a9d
nex0us <camden.alpert7@gmail.com>
2022-09-24 08:52:26 -0500

    api_working

--- Model/server.py
+++ Model/server.py
@@ -1,25 +1,30 @@
 import os
 import base64
+import cv2
+from urllib import request
 import numpy as np
 import tensorflow as tf
 from flask import Flask
+from flask import request
 from keras.models import load_model
 
 
 app = Flask(__name__)
 
-@app.route('/image/<encoded_data>')
-def decodeBase64ToImage(encoded_data):
+@app.route('/image', methods=['POST'])
+def decodeBase64ToImage():
+    encoded_data = request.data
     decoded_data = base64.b64decode((encoded_data))
 
     #write the decoded data back to original format in  file
     img_file = open('image.jpeg', 'wb')
     img_file.write(decoded_data)
-    img_file.close()
+    img = cv2.imread('image.jpeg')
     resize = tf.image.resize(img, (256,256))
 
-    new_model = load_model('imageclassifier.h5')
+    new_model = load_model(os.path.join('Model', 'imageclassifier.h5'))
     yhatnew = new_model.predict(np.expand_dims(resize/255, 0))
+    img_file.close()
 
     if yhatnew > 0.5: 
         return 'Recyclable'
