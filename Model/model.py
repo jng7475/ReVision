f752e6c7b5a79293ee4fc1403747e3f0bbb03dcd
dcsajdak <dcsajdak@gmail.com>
2022-09-24 02:44:49 -0500

    test it camden

--- Model/model.py
+++ Model/model.py
@@ -5,13 +5,38 @@ import imghdr
 from matplotlib import pyplot as plt
 import numpy as np
 
-#os.listdir('trash_data')
-
-# gpus = tf.config.experimental.list_physical_devices('GPU')
+gpus = tf.config.experimental.list_physical_devices('GPU')
 
 #For each of available GPUs set memory consumption growth
-#for gpu in gpus: 
- #   tf.config.experimental.set_memory_growth(gpu, True)
+for gpu in gpus: 
+   tf.config.experimental.set_memory_growth(gpu, True)
 
+
+#Removing Bad Images
 data_dir = 'trash_data'
-#img_exts = ['jpeg','jpg','bmp','png']
+image_exts = ['jpeg','jpg','bmp','png']
+
+for image_class in os.listdir(data_dir): 
+    for image in os.listdir(os.path.join(data_dir, image_class)):
+        image_path = os.path.join(data_dir, image_class, image)
+        try: 
+            img = cv2.imread(image_path)
+            tip = imghdr.what(image_path)
+            if tip not in image_exts: 
+                print('Image not in ext list {}'.format(image_path))
+                os.remove(image_path)
+        except Exception as e: 
+            print('Issue with image {}'.format(image_path))
+
+
+# Load the data            
+data = tf.keras.utils.image_dataset_from_directory(data_dir)
+data_iterator = data.as_numpy_iterator()
+batch = data_iterator.next()
+
+fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+for idx, img in enumerate(batch[0][:4]):
+    ax[idx].imshow(img.astype(int))
+    ax[idx].title.set_text(batch[1][idx])
+    
+plt.show()
