cb25af0395a24b0b7dc6d698ee318670e9e46494
nex0us <camden.alpert7@gmail.com>
2022-09-24 00:54:12 -0500

    Added necessary libraries

--- Model/model.py
+++ Model/model.py
@@ -1,2 +1,17 @@
 import tensorflow as tf
 import os
+import cv2
+import imghdr
+from matplotlib import pyplot as plt
+import numpy as np
+
+#os.listdir('trash_data')
+
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+
+#For each of available GPUs set memory consumption growth
+#for gpu in gpus: 
+ #   tf.config.experimental.set_memory_growth(gpu, True)
+
+data_dir = 'trash_data'
+#img_exts = ['jpeg','jpg','bmp','png']
